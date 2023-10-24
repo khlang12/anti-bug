41060e748717ccbf50de6ea25e2553e56170c84c
bl0ckp1n9 <blockp1n9@gmail.com>
2023-10-25 03:58:39 +0900

    feat: blockchain and node

---
+++ src/blockchain/blockchain.ts
@@ -0,0 +1,42 @@
+import { Block } from "@ethereumjs/block";
+import { Common } from "@ethereumjs/common";
+import { RunTxResult } from "@ethereumjs/vm";
+
+export default class AntibugChain {
+  private common: Common;
+  private chain: Block[];
+  private blockByNumber: Map<bigint, Block> = new Map();
+  private receiptByTxHash: Map<string, any> = new Map();
+  constructor({
+    common,
+    genesisBlock,
+  }: {
+    common: Common;
+    genesisBlock: Block;
+  }) {
+    this.common = common;
+    this.chain = [genesisBlock];
+    this.blockByNumber.set(genesisBlock.header.number, genesisBlock);
+  }
+
+  public getLatestBlock(): Block {
+    return this.chain[this.chain.length - 1];
+  }
+
+  public getBlockByNumber(number: bigint): Block | undefined {
+    return this.blockByNumber.get(number);
+  }
+
+  public getReceiptByTxHash(txHash: string): any | undefined {
+    return this.receiptByTxHash.get(txHash);
+  }
+
+  public putBlock(block: Block) {
+    this.chain.push(block);
+    this.blockByNumber.set(block.header.number, block);
+  }
+
+  public putReceipt(txHash: string, receipt: RunTxResult) {
+    this.receiptByTxHash.set(txHash, receipt);
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,14 @@
 import * as vscode from "vscode";
 
-import { Common } from "@ethereumjs/common";
+import { Block } from "@ethereumjs/block";
+import { Common, Chain, Hardfork } from "@ethereumjs/common";
 import { VM } from "@ethereumjs/vm";
 import { ViewProvider } from "./provider/view-provider";
-import { makeGenesisState } from "./util";
+import { makeGenesisState, privateKeyToAddress } from "./util";
+import { DEFAULT_ACCOUNTS } from "./util/config";
+import { FeeMarketEIP1559Transaction } from "@ethereumjs/tx";
+import { hexToBytes } from "@ethereumjs/util";
+import AntibugNode from "./blockchain/node";
 
 export function activate(context: vscode.ExtensionContext) {
   const primaryPanelInteractionProvider = new ViewProvider({
@@ -25,23 +30,79 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 async function interactionListener(data: { type: string; value: any }) {
-  const common = Common.custom(
-    {
-      chainId: 1234,
-      networkId: 4567,
-    },
-    {
-      hardfork: "shanghai",
-      eips: [1559],
-    }
+  // const common = Common.custom(
+  //   {
+  //     chainId: Chain.Mainnet,
+  //     networkId: Chain.Mainnet,
+  //     genesis: {
+  //       timestamp: "0x0",
+  //       gasLimit: 300000000,
+  //       difficulty: 0,
+  //       nonce: "0x0000000000000000",
+  //       extraData: "0x",
+  //     },
+  //   },
+  //   {
+  //     hardfork: Hardfork.Shanghai,
+  //     eips: [1559, 4895],
+  //   }
+  // );
+
+  // const vm = await VM.create({
+  //   common,
+  //   activatePrecompiles: true,
+  //   genesisState: makeGenesisState(),
+  // });
+
+  const antibugNode = await AntibugNode.create();
+
+  console.log(antibugNode.blockchain.getBlockByNumber(0n));
+  const accountAddress1 = privateKeyToAddress(DEFAULT_ACCOUNTS[0].privateKey);
+  const accountAddress2 = privateKeyToAddress(DEFAULT_ACCOUNTS[1].privateKey);
+
+  const sendTx = {
+    gasLimit: 21000,
+    value: 1000000000000000000n,
+    to: accountAddress2.toString(),
+    maxFeePerGas: 7n,
+  };
+
+  let eip1559Tx = FeeMarketEIP1559Transaction.fromTxData(sendTx).sign(
+    hexToBytes(DEFAULT_ACCOUNTS[0].privateKey)
   );
 
-  const vm = await VM.create({
-    common,
-    activatePrecompiles: true,
-    genesisState: makeGenesisState(),
-  });
+  await antibugNode.mine(eip1559Tx);
 
+  console.log(
+    (await antibugNode.getBalance(accountAddress1.toString())).toString()
+  );
+  console.log(
+    (await antibugNode.getBalance(accountAddress2.toString())).toString()
+  );
+
+  const sendTx2 = {
+    gasLimit: 21000,
+    value: 1000000000000000000n,
+    to: accountAddress2.toString(),
+    maxFeePerGas: 7n,
+    nonce: 1,
+  };
+
+  eip1559Tx = FeeMarketEIP1559Transaction.fromTxData(sendTx2, {
+    common: antibugNode.common,
+  }).sign(hexToBytes(DEFAULT_ACCOUNTS[0].privateKey));
+  await antibugNode.mine(eip1559Tx);
+
+  console.log(
+    (await antibugNode.getBalance(accountAddress1.toString())).toString()
+  );
+  console.log(
+    (await antibugNode.getBalance(accountAddress2.toString())).toString()
+  );
+  // console.log("stateAccount1After", stateAccount1After?.balance.toString());
+  // console.log("stateAccount2After", stateAccount2After?.balance.toString());
+
+  // console.log(await vm.blockchain.getBlock(1));
   switch (data.type) {
     case "sendEth": {
       const panel = vscode.window.createWebviewPanel(
