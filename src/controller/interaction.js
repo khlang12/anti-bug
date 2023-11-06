61853ff786a61278831ee7c00a54622ca489eb49
Kahyun Lim <kahyun2013@gmail.com>
2023-11-06 20:02:02 +0900

    [deploy Sidebar] - chain → 어차피 private chain 사용할 예정이니 disable - address → selector로 수정 (private chain address 찾아옴) - contract → selector로 수정 (open sol file 찾아옴) - value 단위 → selector로 수정 (ETH, Wei, Gwei, Finney)

--- src/controller/interaction.js
+++ src/controller/interaction.js
@@ -1,6 +1,9 @@
 (function () {
   const vscode = acquireVsCodeApi();
 
+  // sidebar 실행과 동시에 getSolFiles로 Listener 실행
+  vscode.postMessage({ type: "getSolFiles", value: null });
+
   const sendInteractionForm = document.querySelector(".send-interaction");
   const addressSelect = document.querySelector(".send-interaction__address");
   const toInput = document.querySelector(".send-interaction__to");
@@ -23,22 +26,29 @@
   window.addEventListener("message", ({ data }) => {
     switch (data.type) {
       case "changeAddressState": {
+        console.log("addEventListener 실행중...");
         const options = data.value.map((item) => {
           const { address, balance, privateKey } = item;
           const element = document.createElement("option");
           element.value = privateKey;
           element.innerHTML = `${address}(${balance})`;
+          console.log("element...", element);
           return element;
         });
         addressSelect.replaceChildren(...options);
         break;
       }
+      case "solFiles": {
+        const dropdown = document.getElementById("solFileDropdown");
+        dropdown.innerHTML = "";
+
+        data.value.forEach((option) => {
+          const optionElement = document.createElement("option");
+          optionElement.value = option.value;
+          optionElement.textContent = option.text;
+          dropdown.appendChild(optionElement);
+        });
+      }
     }
   });
-
-  // function listenText() {
-  //   const sampleText = "sample Text";
-  //   text.push(sampleText);
-  //   vscode.setState({ text: text });
-  // }
-})();
+}) () ;
--- src/listener/interaction.ts
+++ src/listener/interaction.ts
@@ -1,3 +1,6 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as vscode from "vscode";
 import { FeeMarketEIP1559Transaction } from "@ethereumjs/tx";
 import AntibugNode from "../blockchain/node";
 import { hexToBytes } from "@ethereumjs/util";
@@ -11,6 +14,7 @@ export default async function interactionListener(
 ) {
   switch (data.type) {
     case "sendEth": {
+      console.log("sendEth 실행중...");
       const { fromPrivateKey, value, to } = data.value;
 
       const txData = {
@@ -38,8 +42,8 @@ export default async function interactionListener(
             to === account.address
               ? toBalance.toString()
               : from === account.address
-              ? fromBalance.toString()
-              : account.balance.toString(),
+                ? fromBalance.toString()
+                : account.balance.toString(),
         };
       });
 
@@ -47,6 +51,49 @@ export default async function interactionListener(
         type: "changeAddressState",
         value: accounts,
       });
+      break;
+    }
+    case "getSolFiles": {
+      console.log("getSolFiles 실행중...");
+      const workspaceFolders = vscode.workspace.workspaceFolders;
+      if (workspaceFolders && workspaceFolders.length > 0) {
+        const workspaceFolder = workspaceFolders[0];
+        const workspaceFolderPath = workspaceFolder.uri.fsPath;
+        const solFiles = await findSolFiles(workspaceFolderPath);
+        this.view.webview.postMessage({
+          type: "solFiles",
+          value: solFiles,
+        });
+      } else {
+        console.error("No workspace folders found.");
+      }
+      break;
     }
   }
 }
+
+async function findSolFiles(folderPath: string): Promise<{ value: string; text: string }[]> {
+  vscode.window.showInformationMessage("findSolFiles 실행중...");
+  const solFiles: { value: string; text: string }[] = [];
+  async function findFilesRecursively(folderPath: string) {
+    const entries = await fs.promises.readdir(folderPath);
+    for (const entry of entries) {
+      const entryPath = path.join(folderPath, entry);
+      const stats = await fs.promises.stat(entryPath);
+      if (stats.isDirectory()) {
+        await findFilesRecursively(entryPath);
+      } else if (path.extname(entry) === ".sol") {
+        solFiles.push({ value: entryPath, text: entry });
+      }
+    }
+  }
+  await findFilesRecursively(folderPath);
+  return solFiles;
+}
+
+
+
+
+
+
+
